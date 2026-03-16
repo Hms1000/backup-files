2c15fd29b63940964cea2e969115872776f3849b
Hms1000 <hmstakai@gmail.com>
2026-03-16 07:44:30 +0200

    fixed some vim issues, had trouble with a swap file

--- src/main.py
+++ src/main.py
@@ -2,25 +2,20 @@ from fastapi import FastAPI
 from pathlib import Path
 from backup_files import backup_file
 
-# fastapi client 
 app = FastAPI()
 
-# checking if the api is accessible
 @app.get("/")
 def home():
     return {"status": "Backup service running..."}
 
-# perfoming the backup
 @app.post("/backup")
-def post_backup(path_to_backup:str, destination: str):
-    
+def post_backup(path_to_backup: str, destination: str):
     backup_file(
-            Path(path_to_backup),
-            Path(destination)
-            )
+        Path(path_to_backup),
+        Path(destination)
+    )
     return {"status": "backup completed"}
 
-# simple healthcheck
 @app.get("/health")
 def healthcheck():
     return {"status": "healthy"}
